06f572410085876b2f0a4e7c565cbf6000ddb248
deveshaha <devesh_hanumante@outlook.com>
2022-11-16 21:59:54 +0100

    update

--- src/main/java/com/ej3/ejercicio3addressapp/util/DateUtil.java
+++ src/main/java/com/ej3/ejercicio3addressapp/util/DateUtil.java
@@ -6,20 +6,16 @@ import java.time.format.DateTimeParseException;
 
 public class DateUtil {
 
-    /** The date pattern that is used for conversion. Change as you wish. */
+    /**
+     * This class is used to format and parse dates.
+     * The formatter is used to convert a String to a LocalDate and vice versa.
+     */
+
     private static final String DATE_PATTERN = "dd.MM.yyyy";
 
-    /** The date formatter. */
     private static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern(DATE_PATTERN);
 
-    /**
-     * Returns the given date as a well formatted String. The above defined
-     * {@link DateUtil#DATE_PATTERN} is used.
-     *
-     * @param date the date to be returned as a string
-     * @return formatted string
-     */
     public static String format(LocalDate date) {
         if (date == null) {
             return null;
@@ -27,15 +23,6 @@ public class DateUtil {
         return DATE_FORMATTER.format(date);
     }
 
-    /**
-     * Converts a String in the format of the defined {@link DateUtil#DATE_PATTERN}
-     * to a {@link LocalDate} object.
-     *
-     * Returns null if the String could not be converted.
-     *
-     * @param dateString the date as String
-     * @return the date object or null if it could not be converted
-     */
     public static LocalDate parse(String dateString) {
         try {
             return DATE_FORMATTER.parse(dateString, LocalDate::from);
@@ -44,12 +31,6 @@ public class DateUtil {
         }
     }
 
-    /**
-     * Checks the String whether it is a valid date.
-     *
-     * @param dateString
-     * @return true if the String is a valid date
-     */
     public static boolean validDate(String dateString) {
         // Try to parse the String.
         return DateUtil.parse(dateString) != null;
